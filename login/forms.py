ba34423686d0e2e70de49ae6ad9e738322c5c6c9
Viktoriia Homelska <viktoriiahomelska@MacBook-Pro-Viktoriia.local>
2025-05-13 12:02:28 +0300

    Auth form static and structure added

--- login/forms.py
+++ login/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class RegistrationForm(forms.Form):
-    email = forms.EmailField(label="Email", max_length=255, widget = forms.EmailInput)
+    email = forms.EmailField(label="Електронна пошта", max_length=255, widget = forms.EmailInput)
     password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
-    confirm_password = forms.CharField(label="Підтвердження паролю", widget=forms.PasswordInput)
-    confirm_code = forms.CharField(label = 'Підтвердження коду реєстрації', max_length=6, widget = forms.TextInput(attrs= {'id': 'regInput'}), required= False)
+    confirm_password = forms.CharField(label="Підтверди пароль", widget=forms.PasswordInput)
+    confirm_code = forms.CharField(label = 'Підтверди код реєстрації', max_length=6, widget = forms.TextInput(attrs= {'id': 'regInput'}), required= False)
